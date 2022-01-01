c9f27431feed39ef710e09e004d562949e914733
Lucas <ljoel9870@hwdsb.on.ca>
2022-01-01 17:34:30 -0500

    fix

--- EZInstaller/src/gui.py
+++ EZInstaller/src/gui.py
@@ -144,14 +144,27 @@ def submit():
     fileName = file.headers["Content-Disposition"].split("filename=")[1]
     print(f"fileName: {fileName}")
 
-    if not fileName.endswith(".exe" or ".zip" or ".rar" or ".msi"):
-        print("Fail")
+    append_to_output_box("Checking file type..." + "\n")
+    if fileName.endswith(".exe"):
+        isExe = True
+        append_to_output_box("File is an executable." + "\n")
+    elif fileName.endswith(".zip"):
+        isZip = True
+        append_to_output_box("File is a zip file." + "\n")
+    elif fileName.endswith(".rar"):
+        isRar = True
+        append_to_output_box("File is a rar file." + "\n")
+    elif fileName.endswith(".msi"):
+        isMsi = True
+        append_to_output_box("File is an MSI file." + "\n")
     else:
-        print("Success")
+        isOther = True
+        append_to_output_box("File is not an executable, zip, rar, or MSI file." + "\n")
+        time.sleep(5)
+        sys.exit()
 
-    if not fileName.endswith(".exe" or ".zip" or ".rar" or ".msi"):
-        append_to_output_box("Error: " + "File must have an extension." + "\n")
-        append_to_output_box("Please check the URL and try again." + "\n")
+    if isOther == True:
+        append_to_output_box("Error: File is invalid \n")
         time.sleep(5)
         sys.exit()
     append_to_output_box(f"File found, download {fileName} to {storageLocation}" + "\n")
@@ -176,27 +189,6 @@ def submit():
                     append_to_output_box("Download complete." + "\n")
                     f.close()
                     break
-
-    append_to_output_box("Checking file type..." + "\n")
-    type = fileName.split(".")[-1]
-    append_to_output_box(f"File type: {type}" + "\n")
-    if type == "exe":
-        isExe = True
-        append_to_output_box("File is an executable." + "\n")
-    elif type == "zip":
-        isZip = True
-        append_to_output_box("File is a zip file." + "\n")
-    elif type == "rar":
-        isRar = True
-        append_to_output_box("File is a rar file." + "\n")
-    elif type == "msi":
-        isMsi = True
-        append_to_output_box("File is an MSI file." + "\n")
-    else:
-        isOther = True
-        append_to_output_box("File is not an executable, zip, rar, or MSI file." + "\n")
-        time.sleep(5)
-        sys.exit()
 
     if isExe == True:
         append_to_output_box("Running file..." + "\n")
